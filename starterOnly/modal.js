77563320969294346bf0f3108cb81e8a87567a5e
ykrhrlpz <yukarihoralopez@gmail.com>
2021-04-03 18:30:21 +0900

    Created Thank you message modal.

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -11,6 +11,10 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+const submitBtn = document.querySelector("btn-submit");
+const modalBody = document.querySelector(".modal-body");
+const modalContent = document.querySelector(".content");
+
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -20,4 +24,34 @@ function launchModal() {
   modalbg.style.display = "block";
 }
 
+let container = document.createElement("div");
+let thankyouMessage = document.createElement("p");
+let closeButton = document.createElement("button");
+thankyouMessage.textContent = "Thank you for submitting your registration details";
+closeButton.textContent = "Close";
+modalContent.removeChild(modalBody);
+modalContent.appendChild(container);
+container.appendChild(thankyouMessage);
+modalContent.appendChild(closeButton);
 
+console.log(modalBody);
+container.classList.add("thankyou");
+closeButton.classList.add("button", "btn-close");
+container.classList.add("container");
+
+// submitBtn.addEventListener("submit", () => {
+//   modalBody.text
+// })
+
+
+//Referrence
+// FormValidation() {}
+// SwitchModalContent() {}
+
+// SubmitAcion()
+// {
+//   FormValidation()
+//   .then(() => SwitchModalContent())
+//   .then(() => setTimeout(() => CloseModal(), 2000))
+//   .catch(() => "Sorry, something went wrong.")
+// }
