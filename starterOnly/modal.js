ac324ea9723f5ee745df3dffbef168aba8d97614
ykrhrlpz <yukarihoralopez@gmail.com>
2021-04-10 20:46:36 +0900

    Validation for First name, Last name and Email.

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -11,7 +11,8 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
-const form = document.querySelector("#signup-form");
+const form = document.getElementById("signup-form");
+
 const submitBtn = document.querySelector(".btn-submit");
 const modalBody = document.querySelector(".modal-body");
 const modalContent = document.querySelector(".content");
@@ -55,273 +56,48 @@ closeIcon.addEventListener("click", () => {
   closeModal();
 });
 
-// Thank you messagge modal
-// thankyouMessage.textContent = "Thank you for submitting your registration details";
-// closeButton.textContent = "Close";
-// modalContent.removeChild(modalBody);
-// modalContent.appendChild(container);
-// container.appendChild(thankyouMessage);
-// modalContent.appendChild(closeButton);
+const inputs = document.querySelectorAll("form input");
 
-// console.log(modalBody);
-// container.classList.add("thankyou");
-// closeButton.classList.add("button", "btn-close");
-// container.classList.add("container");
+console.log(inputs);
 
-//First Name Input Validation
-
-const validateFirstLastNameInputs = (value) => 
-{
-  
-  if (!value || value.length < 2)
-  {
-    console.log("false");
-
-    firstNameInput.style.backgroundColor = "pink";
-    return false;
-  }
-  else
-  {
-    console.log("true");
-    firstNameInput.style.backgroundColor = "green";
-    return true;
-  }
-};
-
-// Form Validation for first name
-firstNameInput.addEventListener("input", () => 
-{
-  validateFirstLastNameInputs(firstNameInput.value);
-
-  // if (!event.target.value || event.target.value.length < 2)
-  // {
-  //   console.log("error");
-  //   firstNameInput.style.backgroundColor = "pink";
-
-  //   return false;
-  // }
-  // else
-  // {
-  //   firstNameInput.style.backgroundColor = "green";
-  
-  //   return true;
-  // }
-
-
-});
-
-// Form Validation for last name
-// lastNameInput.addEventListener("input", (event) => 
-// {
-//   if (!event.target.value || event.target.value.length < 2)
-//   {
-//     console.log("error");
-//     lastNameInput.style.backgroundColor = "pink";
-
-//     return false;
-//   }
-//   else if (event.target.value.length >= 2 )
-//   {
-//     lastNameInput.style.backgroundColor = "green";
-  
-//     return true;
-//   }
-// });
-
-// Form Validation for email adress
-
-// const validateEmail = (email)  => {
-//   const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-//   return re.test(email.toLowerCase());
-// }
-// emailInput.addEventListener("input", (event) => 
-// {
-//   if (!event.target.value || event.target.value.length < 2)
-//   {
-//     console.log("error");
-//     emailInput.style.backgroundColor = "pink";
-
-//     return false;
-//   }
-//   else if (event.target.value.length >= 2 )
-//   {
-//     emailInput.style.backgroundColor = "green";
-  
-//     return true;
-//   }
-// });
-
-
-
-// firstNameInput.addEventListener("input", (event) => 
-// {
-//     if (event.target.value.length > 2)
-//     {
-//       submitBtn.removeAttribute("disabled");
-//       document.getElementById("error-msg-first-name").classList.add("hidden");
-//       // document.getElementById("error-msg-first-name").classList.remove("hidden");
-//     }
-//     else
-//     {
-//       document.getElementById("error-msg-first-name").classList.remove("hidden");
-//       // document.getElementById("error-msg-first-name").classList.add("hidden");
-//       submitBtn.setAttribute("disabled", "true");
-//     }
-// });
-
-// //Last Name Input Validation
-
-// lastNameInput.addEventListener("input", (event) => 
-// {
-//     if (event.target.value)
-//     {
-//       if (event.target.value.length >= 2 )
-//       {
-//         submitBtn.removeAttribute("disabled");
-//         document.getElementById("error-msg-first-name").classList.add("hidden");
-//       }
-//       else
-//       {
-//         submitBtn.setAttribute("disabled", "true");
-//         document.getElementById("error-msg-first-name").classList.add("hidden");
-//       }
-//     }
-// });
-
-// Element is the element to be checked
-// expectedLenght is the length of the string
-// Strict is a boolean, if strict is true then the length 
-// has to be stricly expectedLenght and not above.
-// const lengthValidation = (element, expectedLength, strict=false) => 
-// {
-//   strict 
-//   ?
-//     element.length == expectedLength ?  true : false
-//   :
-//     element.length >= expectedLength ?  true : false
-// }
-
-// const validate = () => 
-// {
-//   if (!firstNameInput.value || firstNameInput.value.length < 2 )
-//   {
-//     alert("error")
-//     return false;
-//   }
-//   else
-//   {
-//     return true;
-//   }
-// }
-
-// const FormValidation = () => 
-// {
-//   validateFirstNameInput = lengthValidation(firstNameInput)
-//   validateLastNameInput = lengthValidation(lastNameInput)
-  
-//   emailInput
-//   birthDateInput
-//   quantityInput
-//   locationRadioButtons
-//   checkboxOne
-//   checkboxTwo
-
-//   //if all the validation went to true then return true
-// }
-
-
-
-
-//Function which shows the warning message when user enters invalid inputs.
-// const showErrorMessage = (userInput) => 
-// {
-//   switch (first)
-//   {
-//     document.getElementById("error-msg-first-name").classList.remove("hidden");
-//   }
-//   if (last)
-//   {
-//     document.getElementById("error-msg-last-name").classList.remove("hidden");
-//   }
-//   if (email)
-//   {
-//     document.getElementById("error-msg-email").classList.remove("hidden");
-//   }
-//   if (birthdate)
-//   {
-//     document.getElementById("error-msg-birthDate").classList.remove("hidden");
-//   }
-//   if (location)
-//   {
-//     document.getElementById("error-msg-location").classList.remove("hidden");
-//   }
-//   if (checkbox)
-//   {
-//     document.getElementById("error-msg-checkbox").classList.remove("hidden");
-//   }
-  
-    
-// }
-
-
-
-
-//later use promise to structure the code, form validation -> show message ?
-
-
-
-
-
-//Radio button in Modal
-//Radio Button Event Listener
-
-// for (let i = 0; i < locationRadioButtons.length; i++)
-// {
-//   locationRadioButtons[i].addEventListener("change", (event) => {
-
-//   });
-// }
-
-//Checkbox in Modal
-//Checkbox Event Listener
-// document.getElementById("checkbox1").addEventListener("change", (event) => {
-//   if (event.target.checked)
-//   {
-//     //write function
-//   }
-// })
-
-
-//Form Submit Button Event Listener
-form.addEventListener("submit", (event) => 
+form.addEventListener("submit", event => 
 {
   event.preventDefault();
-  submitBtn.removeAttribute("disabled");
-  // validateInputs();
-  //Insert the Thank you message when User submit successfully.
+  inputs.forEach((input) => 
+  {
+    console.log(`${input.getAttribute("name")}: ${input.value}`)
+    console.log(`${input.getAttribute("name")}: ${input.value.length}`)
+    if (!input.value || input.value.length < 2)
+    {
+      input.parentElement.classList.add("error")
+    }
+    // else if (input.value.length < 2)
+    // {
+    //   console.log("error");
+    //   console.log(input.value.length);
+    //   input.parentElement.classList.add("error")
+    // }
+    else
+    {
+      input.parentElement.classList.remove("error")
+      if (input.type == "email")
+      {
+        if (validateEmail(input.value))
+        {
+          input.parentElement.classList.remove("error")
+        }
+        else
+        {
+          input.parentElement.classList.add("error")
+        }
+      }
+    }
+  })
+})
 
-  // thankyouMessage.textContent = "Thank you for submitting your registration details";
-  // closeButton.textContent = "Close";
-  // modalContent.removeChild(modalBody);
-  // modalContent.appendChild(container);
-  // container.appendChild(thankyouMessage);
-  // modalContent.appendChild(closeButton);
-  // container.classList.add("thankyou");
-  // closeButton.classList.add("button", "btn-close");
-  // container.classList.add("container");
-  // Empty fields once the values are extracted.
-  // form.reset();
-});
+function validateEmail(email) {
+  const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(String(email).toLowerCase());
+}
 
-
-//Referrence
-// FormValidation() {}
-// SwitchModalContent() {}
-
-// SubmitAcion()
-// {
-//   FormValidation()
-//   .then(() => SwitchModalContent())
-//   .then(() => setTimeout(() => CloseModal(), 2000))
-//   .catch(() => "Sorry, something went wrong.")
-// }
+// const validate = () => console.log("VALIDATED")
