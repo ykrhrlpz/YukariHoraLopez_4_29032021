56b1e665eef101c2b0f4c98555f16033e8966423
ykrhrlpz <yukarihoralopez@gmail.com>
2021-04-13 06:50:15 +0900

    Commiting the latest updates

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -16,15 +16,14 @@ const form = document.getElementById("signup-form");
 const submitBtn = document.querySelector(".btn-submit");
 const modalBody = document.querySelector(".modal-body");
 const modalContent = document.querySelector(".content");
-const firstNameInput = document.getElementById("first");
-const lastNameInput = document.getElementById("last");
-const emailInput = document.getElementById("email");
-const birthDateInput = document.getElementById("birthdate");
-const quantityInput = document.getElementById("quantity");
+// const firstNameInput = document.getElementById("first");
+// const lastNameInput = document.getElementById("last");
+// const emailInput = document.getElementById("email");
+// const birthDateInput = document.getElementById("birthdate");
+
 const closeIcon = document.querySelector(".close");
 
-const checkboxOne = document.getElementById("checkbox1");
-const checkboxTwo = document.getElementById("checkbox2");
+
 
 // Thank you messagge modal
 let container = document.createElement("div");
@@ -56,12 +55,89 @@ closeIcon.addEventListener("click", () => {
   closeModal();
 });
 
-const inputs = document.querySelectorAll("form input");
 
-console.log(inputs);
+
+//Function for Email Validation
+function validateEmail(email) {
+  const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(String(email).toLowerCase());
+}
+
+//Function for Dropdown Menu Validation
+const ValidateDropdownMenu = (input) =>
+{
+  const quantityInput = document.getElementById("quantity");
+console.log(quantityInput);
+  if (!input.value)
+  {
+    console.log("false");
+    return false;
+  }
+console.log("true");
+  return true;
+}
+
+
+//Function for Radio Button Validation 
+const validateRadioButton = () => 
+{
+  const locationRadioButtons = document.getElementsByName("location");
+console.log(locationRadioButtons);
+  for (let i = 0; i < locationRadioButtons.length; i++)
+  {
+    if (locationRadioButtons[i].checked)
+    {
+      console.log("true");
+      return true;
+    }
+    else
+    {
+      console.log("false");
+      return false;
+    }
+   
+  }
+}
+
+
+//Function for Checkbox Validation 
+const validateCheckbox = () =>
+{
+  const checkboxOne = document.getElementById("checkbox1");
+  if (checkboxOne.checked)
+  {
+    return true;
+  }
+  
+  else
+  {
+    return false;
+  }
+}
+
+
+
+// Function to show Thank you messagge after the form is submitted successfully.
+const showSubmitSuccessMsg = () =>
+{
+  thankyouMessage.textContent = "Thank you for submitting your registration details";
+  closeButton.textContent = "Close";
+  modalContent.removeChild(modalBody);
+  modalContent.appendChild(container);
+  container.appendChild(thankyouMessage);
+  modalContent.appendChild(closeButton);
+  
+  container.classList.add("thankyou");
+  closeButton.classList.add("button", "btn-close");
+  container.classList.add("container");
+}
+
+
 
 form.addEventListener("submit", event => 
 {
+  const inputs = document.querySelectorAll("form input");
+
   event.preventDefault();
   inputs.forEach((input) => 
   {
@@ -73,6 +149,7 @@ form.addEventListener("submit", event =>
     else
     {
       input.parentElement.classList.remove("error")
+     
       if (input.type == "email")
       {
         if (validateEmail(input.value))
@@ -84,6 +161,19 @@ form.addEventListener("submit", event =>
           input.parentElement.classList.add("error")
         }
       }
+
+      else if (input.type == "number")
+      {
+        if (ValidateDropdownMenu(input.value))
+        {
+          input.parentElement.classList.remove("error")
+        }
+        else
+        {
+          input.parentElement.classList.add("error")
+        }
+      }
+
       else if (input.type == "radio")
       {
         if (validateRadioButton())
@@ -95,28 +185,18 @@ form.addEventListener("submit", event =>
           input.parentElement.classList.add("error")
         }
       }
+
+      else if (input.type == "checkbox")
+      {
+        if (validateCheckbox())
+        {
+          input.parentElement.classList.remove("error")
+        }
+        else
+        {
+          input.parentElement.classList.add("error")
+        }
+      }
     }
   })
 })
-
-
-//Function for Email Validation
-function validateEmail(email) {
-  const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  return re.test(String(email).toLowerCase());
-}
-
-//Function for Radio Button Validation 
-const validateRadioButton = () => 
-{
-  let valid = false;
-  const locationRadioButtons = document.getElementsByName("location");
-
-  for (let i = 0; i < locationRadioButtons.length; i++)
-  {
-    if (locationRadioButtons[i].checked)
-    {
-      return true;
-    }
-  }
-}
